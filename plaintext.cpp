e1617a06ca12f5083de0348a34644982cd962813
YunanZhu <yunan.zhu@foxmail.com>
2022-05-17 06:54:16 +0000

    Clean plaintext.h and plaintext.cpp.

--- plaintext.cpp
+++ plaintext.cpp
@@ -11,63 +11,51 @@
 #include "utils.h"
 #include "plaintext.h"
 
-namespace ngtcp2_plaintext
-{
 /**
- * 拷贝自 ngtcp2 库的 “lib/ngtcp2_crypto.h”。
- *
- * NGTCP2_INITIAL_AEAD_OVERHEAD is an overhead of AEAD used by Initial packets.
- * Because QUIC uses AEAD_AES_128_GCM, the overhead is 16 bytes.
- */
-#define NGTCP2_INITIAL_AEAD_OVERHEAD 16
-
-/**
- * 拷贝自 ngtcp2 库的 “tests/ngtcp2_test_helper.h”。
- *
+ * Ref: "ngtcp2_repo/tests/ngtcp2_test_helper.h".
  * NGTCP2_FAKE_AEAD_OVERHEAD is AEAD overhead used in unit tests.
  * Because we use the same encryption/decryption function for both
- * handshake and post handshake packets, we have to use AEAD overhead
+ * handshake and post-handshake packets, we have to use AEAD overhead
  * used in handshake packets.
  */
 #define NGTCP2_FAKE_AEAD_OVERHEAD NGTCP2_INITIAL_AEAD_OVERHEAD
 
 /**
+ * Ref: "ngtcp2_repo/tests/ngtcp2_test_helper.h".
  * NGTCP2_FAKE_HP_MASK is a header protection mask used in unit tests.
  */
 #define NGTCP2_FAKE_HP_MASK "\x00\x00\x00\x00\x00"
 
-    static const uint8_t null_secret[32] = {0};
-    static const uint8_t null_iv[16] = {0};
-    static const uint8_t null_data[4096] = {0};
+namespace ngtcp2_plaintext
+{
+    const uint8_t null_secret[32] = {0};
+    const uint8_t null_iv[16] = {0};
+    const uint8_t null_data[4096] = {0};
 
     /**
-     * 将目前 |conn| 中唯一一个 SCID 设置为“已使用”状态。
+     * Ref: "ngtcp2_repo/tests/ngtcp2_conn_test.c".
+     * 将目前 `conn` 中唯一一个 SCID 设置为 “已使用” 的状态。
      */
     void conn_set_scid_used(ngtcp2_conn *conn)
     {
-        assert(1 == ngtcp2_ksl_len(&conn->scid.set)); // NOTE: Add extern "C" to its declaration.
+        assert(1 == ngtcp2_ksl_len(&conn->scid.set)); // Add extern "C" to `ngtcp2_ksl_len` declaration.
 
-        ngtcp2_ksl_it it = ngtcp2_ksl_begin(&conn->scid.set); // NOTE: Add extern "C" to its declaration.
+        ngtcp2_ksl_it it = ngtcp2_ksl_begin(&conn->scid.set); // Add extern "C" to `ngtcp2_ksl_begin` declaration.
         ngtcp2_scid *scid = static_cast<ngtcp2_scid *>(ngtcp2_ksl_it_get(&it));
         scid->flags |= NGTCP2_SCID_FLAG_USED;
         assert(NGTCP2_PQ_BAD_INDEX == scid->pe.index);
 
-        int rv = ngtcp2_pq_push(&conn->scid.used, &scid->pe); // NOTE: Add extern "C" to its declaration.
+        int rv = ngtcp2_pq_push(&conn->scid.used, &scid->pe); // Add extern "C" to `ngtcp2_pq_push` declaration.
         assert(0 == rv);
     }
 
     /**
-     * 摘录总结 lib ngtcp2 doc 中关于 ngtcp2_crypto_ctx 的描述如下：
-     * ngtcp2_crypto_ctx is a convenient structure to bind all crypto related objects in one place.
-     * Use ngtcp2_crypto_ctx_initial() to initialize this struct for Initial packet encryption.
-     * Use ngtcp2_crypto_ctx_tls() to initialize this struct for Handshake and 1RTT packets.
-     *
-     * 本函数应该和 ngtcp2_crypto_ctx_initial() 在使用上是一致的。
-     *
-     * 参考
-     * 1. "tests/ngtcp2_conn_test.c" 中的函数 init_initial_crypto_ctx()
+     * `ngtcp2_crypto_ctx` is a convenient structure to bind all crypto related objects in one place.
+     * Use `ngtcp2_crypto_ctx_initial()` to initialize this struct for Initial packet encryption.
+     * 本函数同 `ngtcp2_crypto_ctx_initial()` 在使用上是一致的。
+     * Ref: "ngtcp2_repo/tests/ngtcp2_conn_test.c", `init_initial_crypto_ctx()`.
      */
-    void pt_crypto_ctx_initial(ngtcp2_crypto_ctx *ctx)
+    void crypto_ctx_initial(ngtcp2_crypto_ctx *ctx)
     {
         memset(ctx, 0, sizeof(*ctx));
 
@@ -78,12 +66,11 @@ namespace ngtcp2_plaintext
     }
 
     /**
-     * 本函数应该和 ngtcp2_crypto_ctx_tls() 在使用上是一致的。
-     *
-     * 参考
-     * 1. "tests/ngtcp2_conn_test.c" 中的函数 init_crypto_ctx
+     * Use `ngtcp2_crypto_ctx_tls()` to initialize this struct for Handshake and 1RTT packets.
+     * 本函数同 `ngtcp2_crypto_ctx_tls()` 在使用上是一致的。
+     * Ref: "ngtcp2_repo/tests/ngtcp2_conn_test.c", `init_crypto_ctx()`.
      */
-    void pt_crypto_ctx_tls(ngtcp2_crypto_ctx *ctx)
+    void crypto_ctx_tls(ngtcp2_crypto_ctx *ctx)
     {
         memset(ctx, 0, sizeof(*ctx));
 
@@ -94,86 +81,78 @@ namespace ngtcp2_plaintext
     }
 
     /**
-     * ngtcp2_callbacks: is invoked when client application asks TLS stack to produce first TLS cryptographic handshake data.
-     *
-     * 参考：
-     * 1. ngtcp2_crypto_client_initial_cb
-     *      ngtcp2_crypto_derive_and_install_initial_key
-     *      ngtcp2_crypto_read_write_crypto_data
-     * 2. "tests/ngtcp2_conn_test.c" client_initial
-     * 3. "tests/ngtcp2_conn_test.c" setup_handshake_client
+     * Ref:
+     *  "ngtcp2_repo/crypto/shared.c", `ngtcp2_crypto_client_initial_cb()`;
+     *  "ngtcp2_repo/tests/ngtcp2_conn_test.c", `client_initial()`;
+     * 由于 plaintext mode 直接跳过 QUIC handshake 阶段，因此实际上本回调函数应当不被调用。
      */
     int client_initial_cb(ngtcp2_conn *conn, void *user_data)
     {
-        printf("Debug: [%s] is called.\n", __func__);
-
         /* Set crypto_ctx for Initial packet encryption. */
         ngtcp2_crypto_ctx crypto_ctx = {0};
-        pt_crypto_ctx_initial(&crypto_ctx);
+        crypto_ctx_initial(&crypto_ctx);
         ngtcp2_conn_set_initial_crypto_ctx(conn, &crypto_ctx);
 
         /* Install packet protection keying materials for Initial packets. */
-        ngtcp2_crypto_aead_ctx aead_ctx = {0};
-        ngtcp2_crypto_cipher_ctx hp_ctx = {0};
+        const ngtcp2_crypto_aead_ctx null_aead_ctx = {0};
+        const ngtcp2_crypto_cipher_ctx null_hp_ctx = {0};
         ngtcp2_conn_install_initial_key(conn,
-                                        &aead_ctx, null_iv, &hp_ctx, // rx
-                                        &aead_ctx, null_iv, &hp_ctx, // tx
+                                        &null_aead_ctx, null_iv, &null_hp_ctx, // rx
+                                        &null_aead_ctx, null_iv, &null_hp_ctx, // tx
                                         sizeof(null_iv));
 
         /* Set AEAD for Retry integrity tag verification. */
         ngtcp2_crypto_aead retry_aead = {0, NGTCP2_FAKE_AEAD_OVERHEAD};
-        ngtcp2_conn_set_retry_aead(conn, &retry_aead, &aead_ctx);
+        ngtcp2_conn_set_retry_aead(conn, &retry_aead, &null_aead_ctx);
 
-        /* Send some meaningless data. */
+        /* Send some meaningless crypto data. */
         ngtcp2_conn_submit_crypto_data(conn, NGTCP2_CRYPTO_LEVEL_INITIAL, null_data, 217);
         return 0;
     }
 
     /**
-     * ngtcp2_callbacks: is invoked when a server receives the first packet from client.
-     *
-     * 参考：
-     * 1. "tests/ngtcp2_conn_test.c" recv_client_initial
+     * Ref:
+     *  "ngtcp2_repo/crypto/shared.c", `ngtcp2_crypto_recv_client_initial_cb()`;
+     *  "ngtcp2_repo/tests/ngtcp2_conn_test.c", `recv_client_initial()`.
+     * 由于 plaintext mode 直接跳过 QUIC handshake 阶段，因此实际上本回调函数应当不被调用。
      */
     int recv_client_initial_cb(ngtcp2_conn *conn, const ngtcp2_cid *dcid, void *user_data)
     {
-        printf("Debug: [%s] is called.\n", __func__);
-
         /* Set crypto_ctx for Initial packet encryption. */
         ngtcp2_crypto_ctx crypto_ctx = {0};
-        pt_crypto_ctx_initial(&crypto_ctx);
+        crypto_ctx_initial(&crypto_ctx);
         ngtcp2_conn_set_initial_crypto_ctx(conn, &crypto_ctx);
 
         /* Install packet protection keying materials for Initial packets. */
-        ngtcp2_crypto_aead_ctx aead_ctx = {0};
-        ngtcp2_crypto_cipher_ctx hp_ctx = {0};
+        const ngtcp2_crypto_aead_ctx null_aead_ctx = {0};
+        const ngtcp2_crypto_cipher_ctx null_hp_ctx = {0};
         ngtcp2_conn_install_initial_key(conn,
-                                        &aead_ctx, null_iv, &hp_ctx, // rx
-                                        &aead_ctx, null_iv, &hp_ctx, // tx
+                                        &null_aead_ctx, null_iv, &null_hp_ctx, // rx
+                                        &null_aead_ctx, null_iv, &null_hp_ctx, // tx
                                         sizeof(null_iv));
 
         /* Set crypto_ctx for Handshake/1RTT packet encryption. */
-        pt_crypto_ctx_tls(&crypto_ctx);
+        crypto_ctx_tls(&crypto_ctx);
         ngtcp2_conn_set_crypto_ctx(conn, &crypto_ctx);
 
         conn->negotiated_version = conn->client_chosen_version;
 
         /* Install packet protection keying materials for encrypting/decrypting Handshake packets. */
-        ngtcp2_conn_install_rx_handshake_key(conn, &aead_ctx, null_iv, sizeof(null_iv), &hp_ctx);
-        ngtcp2_conn_install_tx_handshake_key(conn, &aead_ctx, null_iv, sizeof(null_iv), &hp_ctx);
+        ngtcp2_conn_install_rx_handshake_key(conn, &null_aead_ctx, null_iv, sizeof(null_iv), &null_hp_ctx);
+        ngtcp2_conn_install_tx_handshake_key(conn, &null_aead_ctx, null_iv, sizeof(null_iv), &null_hp_ctx);
 
         return 0;
     }
 
-    // ngtcp2_callbacks: is invoked when cryptographic data (CRYPTO frame, in other words, TLS message) is received.
+    /**
+     * Ref: "ngtcp2_repo/tests/ngtcp2_conn_test.c", `recv_crypto_data()`.
+     */
     int recv_crypto_data_cb(ngtcp2_conn *conn, ngtcp2_crypto_level crypto_level,
                             uint64_t offset, const uint8_t *data, size_t datalen, void *user_data)
     {
-        printf("Debug: [%s] is called.\n", __func__);
         return 0;
     }
 
-    // ngtcp2_callbacks: 当本端需要加密一个 QUIC packet 时会调用本函数。
     int encrypt_cb(uint8_t *dest, const ngtcp2_crypto_aead *aead,
                    const ngtcp2_crypto_aead_ctx *aead_ctx,
                    const uint8_t *plaintext, size_t plaintextlen,
@@ -183,12 +162,11 @@ namespace ngtcp2_plaintext
         if (plaintextlen && plaintext != dest)
             memmove(dest, plaintext, plaintextlen); // 直接将明文拷贝到 dest 中
 
-        memset(dest + plaintextlen, 0, aead->max_overhead); // AEAD encryption overhead 部分全部置零
+        memset(dest + plaintextlen, 0, aead->max_overhead); // AEAD encryption overhead 部分全部置为零
 
         return 0;
     }
 
-    // ngtcp2_callbacks: 当本端需要解密一个 QUIC packet 时会调用本函数。
     int decrypt_cb(uint8_t *dest, const ngtcp2_crypto_aead *aead,
                    const ngtcp2_crypto_aead_ctx *aead_ctx,
                    const uint8_t *ciphertext, size_t ciphertextlen,
@@ -201,7 +179,6 @@ namespace ngtcp2_plaintext
         return 0;
     }
 
-    // ngtcp2_callbacks: is invoked when lib ngtcp2 asks the application to produce mask to encrypt or decrypt packet header.
     int hp_mask_cb(uint8_t *dest, const ngtcp2_crypto_cipher *hp,
                    const ngtcp2_crypto_cipher_ctx *hp_ctx,
                    const uint8_t *sample)
@@ -210,13 +187,14 @@ namespace ngtcp2_plaintext
         return 0;
     }
 
-    // ngtcp2_callbacks: is invoked when a client receives Retry packet. This callback is client only.
     int recv_retry_cb(ngtcp2_conn *conn, const ngtcp2_pkt_hd *hd, void *user_data)
     {
         return 0;
     }
 
-    // ngtcp2_callbacks: is invoked when the lib tells the application it must generate new packet protection keying materials and AEAD cipher context objects with new keys.
+    /**
+     * Ref: "ngtcp2_repo/tests/ngtcp2_conn_test.c", `update_key()`.
+     */
     int update_key_cb(ngtcp2_conn *conn, uint8_t *rx_secret, uint8_t *tx_secret,
                       ngtcp2_crypto_aead_ctx *rx_aead_ctx, uint8_t *rx_iv,
                       ngtcp2_crypto_aead_ctx *tx_aead_ctx, uint8_t *tx_iv,
@@ -225,7 +203,6 @@ namespace ngtcp2_plaintext
     {
         assert(sizeof(null_secret) == secretlen);
 
-        // 这些内容都置为 0xff 时必须的吗？猜测全部置为 0x00 应该也可以才对。#TODO 待探究
         memset(rx_secret, 0xff, sizeof(null_secret));
         memset(tx_secret, 0xff, sizeof(null_secret));
 
@@ -238,47 +215,49 @@ namespace ngtcp2_plaintext
         return 0;
     }
 
-    // ngtcp2_callbacks: deletes a given AEAD cipher context object.
     void delete_crypto_aead_ctx_cb(ngtcp2_conn *conn, ngtcp2_crypto_aead_ctx *aead_ctx, void *user_data)
     {
-        // 由于 aead_ctx->native_handle 应当是一个空指针，因此并不需要 delete，可以考虑使用 assert 测试一下。#TODO
+        // 由于 aead_ctx->native_handle 应当是一个空指针，因此并不需要 delete 。
+        assert(!(aead_ctx->native_handle));
         return;
     }
 
-    // ngtcp2_callbacks: deletes a given cipher context object.
     void delete_crypto_cipher_ctx_cb(ngtcp2_conn *conn, ngtcp2_crypto_cipher_ctx *cipher_ctx, void *user_data)
     {
-        // 由于 cipher_ctx->native_handle 应当是一个空指针，因此并不需要 delete，可以考虑使用 assert 测试一下。#TODO
+        // 由于 cipher_ctx->native_handle 应当是一个空指针，因此并不需要 delete 。
+        assert(!(cipher_ctx->native_handle));
         return;
     }
 
     /**
-     * ngtcp2_callbacks: ask the application for new data that is sent in PATH_CHALLENGE frame.
-     * Application must generate new unpredictable exactly NGTCP2_PATH_CHALLENGE_DATALEN bytes
-     * of random data and store them into the buffer pointed by data.
-     *
-     * PATH_CHALLENGE frame 主要是用于 Path Validation，根据文档要求直接生成一串 random bytes 数据即可。
+     * Application must generate new unpredictable exactly `NGTCP2_PATH_CHALLENGE_DATALEN` bytes
+     * of random data and store them into the buffer pointed by `data`.
+     * Ref:
+     *  "ngtcp2_repo/crypto/boringssl/boringssl.c", `ngtcp2_crypto_get_path_challenge_data_cb()`.
+     *  "ngtcp2_repo/tests/ngtcp2_conn_test.c", `get_path_challenge_data()`.
      */
     int get_path_challenge_data_cb(ngtcp2_conn *conn, uint8_t *data, void *user_data)
     {
+        // memset(data, 0, NGTCP2_PATH_CHALLENGE_DATALEN);
         rand_bytes(data, NGTCP2_PATH_CHALLENGE_DATALEN);
 
         return 0;
     }
 
-    // ngtcp2_callbacks: is invoked when the compatible version negotiation takes place.
-    // For client, it is called when it sees a change in version field of a long header packet.
-    // This callback function might be called multiple times for client.
-    // For server, it is called once when the version is negotiated.
+    /**
+     * Ref:
+     *  "ngtcp2_repo/crypto/shared.c", `ngtcp2_crypto_version_negotiation_cb()`.
+     *  "ngtcp2_repo/tests/ngtcp2_conn_test.c", `version_negotiation()`
+     */
     int version_negotiation_cb(ngtcp2_conn *conn, uint32_t version,
                                const ngtcp2_cid *client_dcid, void *user_data)
     {
-        ngtcp2_crypto_aead_ctx aead_ctx = {0};
-        ngtcp2_crypto_cipher_ctx hp_ctx = {0};
+        ngtcp2_crypto_aead_ctx null_aead_ctx = {0};
+        ngtcp2_crypto_cipher_ctx null_hp_ctx = {0};
 
         ngtcp2_conn_install_vneg_initial_key(conn, version,
-                                             &aead_ctx, null_iv, &hp_ctx,
-                                             &aead_ctx, null_iv, &hp_ctx,
+                                             &null_aead_ctx, null_iv, &null_hp_ctx,
+                                             &null_aead_ctx, null_iv, &null_hp_ctx,
                                              sizeof(null_iv));
 
         return 0;
@@ -289,11 +268,13 @@ namespace ngtcp2_plaintext
 {
     void preset_fixed_dcid_scid(bool is_server, ngtcp2_cid &dcid, ngtcp2_cid &scid)
     {
+        // 代表 client 端的 Connection ID，长度 18 个字节。
         static const uint8_t client_cid[19] = "\xff\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
-                                              "\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xff"; // 用来代表 client 端的 CID，长度 18 个字节
+                                              "\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xff";
 
+        // 代表 server 端的 Connection ID，长度 18 个字节。
         static const uint8_t server_cid[19] = "\xee\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa"
-                                              "\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xee"; // 用来代表 server 端的 CID，长度 18 个字节
+                                              "\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xaa\xee";
 
         if (is_server)
         {
@@ -340,10 +321,8 @@ namespace ngtcp2_plaintext
     {
         ngtcp2_transport_params_default(&params);
 
-        switch (is_server)
+        if (is_server)
         {
-        case true: // server 端
-
             params.initial_max_streams_uni = 0;  // 远端可以创建的并发的单向 stream 的数目。
             params.initial_max_streams_bidi = 5; // 远端可以创建的并发的双向 stream 的数目。
 
@@ -361,11 +340,9 @@ namespace ngtcp2_plaintext
 
             // 设定 connection level 的 flow control window。
             params.initial_max_data = 1024 * 1024;
-
-            break;
-
-        case false: // client 端
-
+        }
+        else
+        {
             params.initial_max_streams_uni = 0;
             params.initial_max_streams_bidi = 0;
 
@@ -374,8 +351,6 @@ namespace ngtcp2_plaintext
             params.initial_max_stream_data_uni = 128 * 1024;
 
             params.initial_max_data = 1024 * 1024;
-
-            break;
         }
     }
 
@@ -413,7 +388,7 @@ namespace ngtcp2_plaintext
 
         /* Set crypto_ctx for Handshake/1RTT packet encryption. */
         ngtcp2_crypto_ctx crypto_ctx = {0};
-        pt_crypto_ctx_tls(&crypto_ctx);
+        crypto_ctx_tls(&crypto_ctx);
         ngtcp2_conn_set_crypto_ctx(conn, &crypto_ctx);
 
         /* Install packet protection keying materials for decrypting incoming Handshake packets. */
@@ -458,7 +433,7 @@ namespace ngtcp2_plaintext
         }
 
         /* 调用 conn_handshake_completed */
-        // 由于我们跳过了 QUIC 握手阶段，因此该函数不会被 ngtcp2 库调用，我们必须手动调用该函数，
+        // 由于我们跳过了 QUIC 握手阶段，因此该函数不会自动被 lib ngtcp2 调用，需要手动调用该函数，
         // 该函数作用就是调用 callbacks.{handshake_completed, extend_max_local_streams_bidi, extend_max_local_streams_uni} 这三个回调函数。
         conn_handshake_completed(conn);
 
--- plaintext.h
+++ plaintext.h
@@ -5,52 +5,80 @@
 
 namespace ngtcp2_plaintext
 {
-    void pt_crypto_ctx_initial(ngtcp2_crypto_ctx *ctx);
-
-    void pt_crypto_ctx_tls(ngtcp2_crypto_ctx *ctx);
-
+    /**
+     * ngtcp2_callbacks: it is invoked when client application asks TLS stack to produce first TLS cryptographic handshake data.
+     */
     int client_initial_cb(ngtcp2_conn *conn, void *user_data);
 
+    /**
+     * ngtcp2_callbacks: it is invoked when a server receives the first packet from client.
+     */
     int recv_client_initial_cb(ngtcp2_conn *conn, const ngtcp2_cid *dcid, void *user_data);
 
+    /**
+     * ngtcp2_callbacks: it is invoked when cryptographic data (CRYPTO frame, in other words, TLS message) is received.
+     */
     int recv_crypto_data_cb(ngtcp2_conn *conn, ngtcp2_crypto_level crypto_level,
                             uint64_t offset, const uint8_t *data, size_t datalen, void *user_data);
 
-    int recv_crypto_data_server_cb(ngtcp2_conn *conn, ngtcp2_crypto_level crypto_level,
-                                   uint64_t offset, const uint8_t *data, size_t datalen, void *user_data);
-
-    int handshake_completed_cb(ngtcp2_conn *conn, void *user_data);
-
+    /**
+     * ngtcp2_callbacks: 当本端需要加密一个 QUIC packet 时会调用本回调函数。
+     */
     int encrypt_cb(uint8_t *dest, const ngtcp2_crypto_aead *aead,
                    const ngtcp2_crypto_aead_ctx *aead_ctx,
                    const uint8_t *plaintext, size_t plaintextlen,
                    const uint8_t *nonce, size_t noncelen,
                    const uint8_t *aad, size_t aadlen);
 
+    /**
+     * ngtcp2_callbacks: 当本端需要解密一个 QUIC packet 时会调用本回调函数。
+     */
     int decrypt_cb(uint8_t *dest, const ngtcp2_crypto_aead *aead,
                    const ngtcp2_crypto_aead_ctx *aead_ctx,
                    const uint8_t *ciphertext, size_t ciphertextlen,
                    const uint8_t *nonce, size_t noncelen,
                    const uint8_t *aad, size_t aadlen);
 
+    /**
+     * ngtcp2_callbacks: it is invoked when libngtcp2 asks the application to produce mask to encrypt/decrypt packet header.
+     */
     int hp_mask_cb(uint8_t *dest, const ngtcp2_crypto_cipher *hp,
                    const ngtcp2_crypto_cipher_ctx *hp_ctx,
                    const uint8_t *sample);
 
+    /**
+     * ngtcp2_callbacks: it is invoked when a client receives Retry packet. This callback is client only.
+     */
     int recv_retry_cb(ngtcp2_conn *conn, const ngtcp2_pkt_hd *hd, void *user_data);
 
+    /**
+     * ngtcp2_callbacks: it is invoked when the lib tells the application
+     * it must generate new packet protection keying materials and AEAD cipher context objects with new keys.
+     */
     int update_key_cb(ngtcp2_conn *conn, uint8_t *rx_secret, uint8_t *tx_secret,
                       ngtcp2_crypto_aead_ctx *rx_aead_ctx, uint8_t *rx_iv,
                       ngtcp2_crypto_aead_ctx *tx_aead_ctx, uint8_t *tx_iv,
                       const uint8_t *current_rx_secret, const uint8_t *current_tx_secret, size_t secretlen,
                       void *user_data);
 
+    /**
+     * ngtcp2_callbacks: it deletes a given AEAD cipher context object.
+     */
     void delete_crypto_aead_ctx_cb(ngtcp2_conn *conn, ngtcp2_crypto_aead_ctx *aead_ctx, void *user_data);
 
+    /**
+     * ngtcp2_callbacks: it deletes a given cipher context object.
+     */
     void delete_crypto_cipher_ctx_cb(ngtcp2_conn *conn, ngtcp2_crypto_cipher_ctx *cipher_ctx, void *user_data);
 
+    /**
+     * ngtcp2_callbacks: libngtcp2 asks the application for new data that is sent in PATH_CHALLENGE frame.
+     */
     int get_path_challenge_data_cb(ngtcp2_conn *conn, uint8_t *data, void *user_data);
 
+    /**
+     * ngtcp2_callbacks: it is invoked when the compatible version negotiation takes place.
+     */
     int version_negotiation_cb(ngtcp2_conn *conn, uint32_t version,
                                const ngtcp2_cid *client_dcid, void *user_data);
 
@@ -59,23 +87,23 @@ namespace ngtcp2_plaintext
 namespace ngtcp2_plaintext
 {
     /**
-     * 由于直接跳过了 QUIC handshake 阶段，因此必须为 client 和 server 端预设 Connection ID。
+     * 由于直接跳过了 QUIC handshake 阶段，因此必须为 client 和 server 端预设固定的 Connection ID。
      */
     void preset_fixed_dcid_scid(bool is_server, ngtcp2_cid &dcid, ngtcp2_cid &scid);
 
     /**
-     * 为 |callbacks| 设置其中 crypto 相关的那些回调函数（注意是明文传输模式下的）。
-     * |isServer| 指明 |callbacks| 将会被用于创建 server 端还是 client 端的 ngtcp2_conn 对象。
+     * 为 `callbacks` 设置其中 crypto 相关的那些回调函数（注意是明文传输模式下的）。
+     * `is_server` 指明 `callbacks` 将会被用于创建 server 端还是 client 端的 ngtcp2_conn 对象。
      */
     void set_ngtcp2_crypto_callbacks(bool is_server, ngtcp2_callbacks &callbacks);
 
     /**
-     * 默认设置 |settings| ，完成后应当可以被直接用于创建 ngtcp2_conn 对象。
+     * 默认设置 `settings`，完成后应当可以被直接用于创建 ngtcp2_conn 对象。
      */
     void set_default_ngtcp2_settings(bool is_server, ngtcp2_settings &settings, ngtcp2_printf log_printf, ngtcp2_tstamp initial_timestamp);
 
     /**
-     * 默认设置 |params|，完成后应当可以被直接用于创建 ngtcp2_conn 对象。
+     * 默认设置 `params`，完成后应当可以被直接用于创建 ngtcp2_conn 对象。
      */
     void set_default_ngtcp2_transport_params(bool is_server, ngtcp2_transport_params &params);
 
